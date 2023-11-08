6537c6dc8668f9fb321bf7d81d9d109153a2a2ac
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-11-08 18:56:00 +0500

    code

--- class3/main.js
+++ class3/main.js
@@ -12,12 +12,33 @@
 // = assign a new value
 // == check the value of number
 // === check the value of a number also check the string
+// >= check the equal or greater value
+// <= check the equal or lesser value
+// ! not operator
+// != not equals
+// && and operator
+// || or operator
 
-let num1 = 5
-let num2 = "5"
+// =        !
+// ==       !=
+// ===      !==
 
-let result = num1 === "5" + num2
+// true                         false
 
-// "5" + "5" = 55
+// 1 any other number           0
 
-console.log(result);
+// "hello"                      ""
+
+// true + true ===  true
+// false + false == true
+// true + false === false
+// false + true === false
+
+// true =====> "asdf" ======> 1234
+// false ====> "" ==========> 0
+
+
+// ++ num1      pre  increment
+// num1 ++      post increment
+// -- num1      pre  decrement
+// num1 --      post decrement
---
+++ class4/main.js
@@ -0,0 +1,20 @@
+
+// task 1
+
+let num1 = 23
+let num2 = 19
+let result1 = num1++ - ++num1 - ++num1 + --num2
+
+
+// task 2
+
+let num3 = 34
+let num4 = 8
+let result2 = num2++ - --num1 + --num2 + num1--
+
+
+// task 3
+
+let num5 = 19
+let num6 = 73
+let result3 = num2-- + ++num2 + ++num1 + --num1 - --num2 + ++num1
