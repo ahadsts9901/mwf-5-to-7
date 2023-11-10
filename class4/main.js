833dfbb9111b621c4fc63a62a30314feb1f376e5
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-11-10 18:50:04 +0500

    code

--- class4/main.js
+++ class4/main.js
@@ -4,17 +4,21 @@
 let num1 = 23
 let num2 = 19
 let result1 = num1++ - ++num1 - ++num1 + --num2
+// -10
 
 
 // task 2
 
 let num3 = 34
 let num4 = 8
-let result2 = num2++ - --num1 + --num2 + num1--
-
+let result2 = num4++ - --num3 + --num4 + num3--
+// 16
 
 // task 3
 
 let num5 = 19
 let num6 = 73
-let result3 = num2-- + ++num2 + ++num1 + --num1 - --num2 + ++num1
+let result3 = num6-- + ++num6 + ++num5 + --num5 - --num6 + ++num5
+console.log(result3);
+
+// 133
---
+++ test1/README.md
@@ -0,0 +1,1 @@
+make a facebook login page
