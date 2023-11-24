9b89b21555b8db59b822a9a3a473cfa37c02fbf5
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-11-24 18:42:38 +0500

    code

---
+++ 12.nested-for-loop-with-array/main.js
@@ -0,0 +1,46 @@
+// let arr = [12, 13, 14, 15, 16, 89, 45, 23] //8
+// console.log("Arr Length : ", arr.length);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+// let arr2 = [
+//     [1, 2, 3],
+//     ["a", "b", "c"],
+//     ["hello", "world", "pencil"]
+// ]
+
+// for (let i = 0; i < arr2.length; i++) {
+
+//     // console.log(arr2[i]);
+
+//     for (let k = 0; k < arr2[i].length; k++) {
+
+//         console.log(arr2[i][k], i, k);
+
+//     }
+// }
+
+
+// let arr3 = [
+//     [["ar1", "ar2", "ar3"], ["ar4", "ar5", "ar6"], ["ar7", "ar8", "ar9"]],
+//     [["ar10", "ar11", "ar12"], ["ar13", "ar14", "ar15"], ["ar16", "ar17", "ar18"]],
+//     [["ar19", "ar20", "ar21"], ["ar22", "ar23", "ar24"], ["ar25", "ar26", "ar27"]],
+// ]
+
+// // first loop
+// for (let i = 0; i < arr3.length; i++) {
+//     console.log(arr3[i]);
+//     // second loop
+//     for (let j = 0; j < arr3[i].length; j++) {
+//         console.log(arr3[i][j]);
+//         // third loop
+//         for (let k = 0; k < arr3[i][j].length; k++) {
+//             console.log(arr3[i][j][k]);
+//         }
+//     }
+// }
+
+document.getElementById("result").innerText = "hello world"
---
+++ 9.for-loop/main.js
@@ -0,0 +1,13 @@
+
+
+
+// initialization => condition => increment or decrement
+//          
+//  
+
+// let fruits = ["mango", "banana", "orange", "apple", "cherry", "pineapple", "grapes", "strawberry", "watermelon", "kharbooza", "cheeku", "anaar"]
+
+// for (let num = 0; num < fruits.length; num++) {
+//     console.log(fruits[num]);
+// }
+
